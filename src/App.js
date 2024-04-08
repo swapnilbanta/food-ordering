89de03cd071fda2255892f6d0ce826b923b6915f
Swapnil Banta <swapnilbanta@gmail.com>
2024-04-08 17:55:48 +0530

    Added the api code

--- src/App.js
+++ src/App.js
@@ -6,48 +6,59 @@ import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {createBrowserRouter,RouterProvider,Outlet} from "react-router-dom"
 import RestaurantMenu from "./components/RestaurantMenu";   
+
 
 const AppLayout = ()=>{
 
     return (
         <div className="app">
-<Header/>
-<Body/>
-{/* <Outlet /> */}
+<Header />
+      <Routes>
+        <Route path="/" element={<Body />}  />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/restaurants/:resId" element={<RestaurantMenu />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
 
 </div>
         )
 
 };
-const foodRouteApp = createBrowserRouter([
-        {
-        path:"/",
-        element:<AppLayout/>,
-        children
-        :[  {
-                path:"/",
-                element:<Body/>
-        },
-        {
-                path:"/about",
-                element:<About/>
-            },
-            {
-                 path:"/contact",
-                 element:<Contact/>
-                     },{
-                        path:"/restaurants/:resId",
-                        element:<RestaurantMenu/>
-                     }
-                ],
-        errorElement:<Error/>
-        },
+// const foodRoute = createBrowserRouter([
+//         {
+//         path:"/",
+//         element:<AppLayout/>,
+//         children
+//         :[  {
+//                 path:"/",
+//                 element:<Body/>
+//         },
+//         {
+//                 path:"/about",
+//                 element:<About/>
+//             },
+//             {
+//                  path:"/contact",
+//                  element:<Contact/>
+//                      },{
+//                         path:"/restaurants/:resId",
+//                         element:<RestaurantMenu/>
+//                      }
+//                 ],
+//         errorElement:<Error/>
+//         },
       
 
-]);
+// ]);
+
+
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render( <RouterProvider router ={foodRouteApp} />);
+root.render( <BrowserRouter>
+        <AppLayout />
+      </BrowserRouter>);
 export default AppLayout;
