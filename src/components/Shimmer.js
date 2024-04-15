14cf20ef3bbc76add89ecd38c60ef6e35e5ad0d4
Swapnil Banta <swapnilbanta@gmail.com>
2024-04-15 16:03:07 +0530

    shimmer ui fixes

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -14,6 +14,14 @@ const Shimmer = ()=>{
           bg-gray-200"></div>
 <div className="m-4 p-4 w-[250px]  h--[250px]
           bg-gray-200"></div>
+          <div className="m-4 p-4 w-[250px]  h--[250px]
+          bg-gray-200"></div>
+          <div className="m-4 p-4 w-[250px]  h--[250px]
+          bg-gray-200"></div>
+          <div className="m-4 p-4 w-[250px]  h--[250px]
+          bg-gray-200"></div>
+          <div className="m-4 p-4 w-[250px]  h--[250px]
+          bg-gray-200"></div>
 
 </div>
 
